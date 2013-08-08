35ccbe9371b17cfcac235e1d4cc7af075648279f
heroandtn3 <heroandtn3@gmail.com>
2013-08-08 17:01:14 +0700

    fix failed move made by Pawn

--- src/com/sangnd/faceme/core/model/chess/Pawn.java
+++ src/com/sangnd/faceme/core/model/chess/Pawn.java
@@ -11,7 +11,6 @@ import com.sangnd.faceme.core.model.ChessPosition;
 
 
 
-
 public class Pawn implements Chess {
 
 	private boolean passedRiver = false;
@@ -26,80 +25,76 @@ public class Pawn implements Chess {
 			return true;
 		return false;
 	}
-
+	
+	@Override
 	public List<ChessPosition> getPosCanMove(ChessPosition current) {
-		List<ChessPosition> pos = new ArrayList<ChessPosition>();
-		ChessPosition CpTemp;
-		int x, y, value, omg, tmpX, tmpY;
-		x = current.getCol();
-		y = current.getRow();
-		if (y <= 4)
-			omg = -1;
-		else
-			omg = 1;
-		passedRiver = CheckPassRiver(current, omg);
-		value = board.getTable()[y][x];
+		List<ChessPosition> list = new ArrayList<ChessPosition>();
+		int row = current.getRow();
+		int col = current.getCol();
+		
+		int side = 1;
+		if (row <= 4) side = -1;
+		
+		passedRiver = CheckPassRiver(current, side);
+		
+		int nextRow;
+		int nextCol;
+		int value = board.getTable()[row][col];
+		
+		
 		if (passedRiver) {
 			
 			// go forward
-			tmpX = x;
-			tmpY = y - omg;
+			nextRow = row + side;
+			nextCol = col;
 			
-			if ((tmpY >= 0)
-					&& (tmpY <= 9)
-					&& ((board.getTable()[tmpY][tmpX] == 0) || (board
-							.getTable()[tmpY][tmpX] * value < 0))) {
-				if (board.getTable()[tmpY][tmpX] * value < 0) {
-					CpTemp = new ChessPosition(tmpX, tmpY, true);
-				} else {
-					CpTemp = new ChessPosition(tmpX, tmpY, false);
+			if (0 <= nextRow && nextRow <= 9) {
+				int nextValue = board.getTable()[nextRow][nextCol];
+				if (nextValue == 0) {
+					list.add(new ChessPosition(nextCol, nextRow, false));
+				} else if (value * nextValue < 0) {
+					list.add(new ChessPosition(nextCol, nextRow, true));
 				}
-				pos.add(CpTemp);
-				
 			}
-			tmpX = x - 1;
-			tmpY = y;
-			if ((tmpX >= 0)
-					&& ((board.getTable()[tmpY][tmpX] == 0) || (board
-							.getTable()[tmpY][tmpX] * value < 0))) {
-
-				if (board.getTable()[tmpY][tmpX] * value < 0) {
-					CpTemp = new ChessPosition(tmpX, tmpY, true);
-				} else {
-					CpTemp = new ChessPosition(tmpX, tmpY, false);
+			
+			// go left
+			nextRow = row;
+			nextCol = col - 1;
+			if (nextCol >= 0) {
+				int nextValue = board.getTable()[nextRow][nextCol];
+				if (nextValue == 0) {
+					list.add(new ChessPosition(nextCol, nextRow, false));
+				} else if (value * nextValue < 0) {
+					list.add(new ChessPosition(nextCol, nextRow, true));
 				}
-				pos.add(CpTemp);
 			}
-
-			tmpX = x + 1;
-			tmpY = y;
-			if ((tmpX <= 8)
-					&& ((board.getTable()[tmpY][tmpX] == 0) || (board
-							.getTable()[tmpY][tmpX] * value < 0))) {
-				if (board.getTable()[tmpY][tmpX] * value < 0) {
-					CpTemp = new ChessPosition(tmpX, tmpY, true);
-				} else {
-					CpTemp = new ChessPosition(tmpX, tmpY, false);
+			
+			// go right
+			nextRow = row;
+			nextCol = col + 1;
+			if (nextCol <= 8) {
+				int nextValue = board.getTable()[nextRow][nextCol];
+				if (nextValue == 0) {
+					list.add(new ChessPosition(nextCol, nextRow, false));
+				} else if (value * nextValue < 0) {
+					list.add(new ChessPosition(nextCol, nextRow, true));
 				}
-				pos.add(CpTemp);
 			}
 		} else {
-			tmpX = x;
-			tmpY = y - omg;
-			if ((tmpY >= 0)
-					&& ((board.getTable()[tmpY][tmpX] == 0) || (board
-							.getTable()[tmpY][tmpX] * value < 0))) {
-				if (board.getTable()[tmpY][tmpX] * value < 0) {
-					CpTemp = new ChessPosition(tmpX, tmpY, true);
-				} else {
-					CpTemp = new ChessPosition(tmpX, tmpY, false);
+			// not passed river yet
+			// go forward only
+			nextRow = row - side;
+			nextCol = col;
+			if (0 <= nextRow && nextRow <= 9) {
+				int nextValue = board.getTable()[nextRow][nextCol];
+				if (nextValue == 0) {
+					list.add(new ChessPosition(nextCol, nextRow, false));
+				} else if (value * nextValue < 0) {
+					list.add(new ChessPosition(nextCol, nextRow, true));
 				}
-				pos.add(CpTemp);
 			}
-
 		}
-
-		return pos;
+		return list;
 	}
 
 	@Override
